1c68ddf778147bf31814cb4bc9fea289ac30c9d5
tik9 <len-win-user>
2020-12-20 12:47:50 +0100

    New Commit

--- prep_read.ps1
+++ prep_read.ps1
@@ -1,40 +1,53 @@
+
+$title='Table of Contents'
+
 $last = get-childitem . | sort-object lastwritetime | select-object -last 1
 
 $last=$last.basename.replace('_',' ')
 $TextInfo = (Get-Culture).TextInfo
 $last=$TextInfo.totitlecase($last)
 
-echo "('$last','ok')" |clip
+# echo "('$last','ok')" |clip
+
+$lines = Get-Content .
+$pos = [array]::indexof($lines, $lines -match ')') 
+$newLines = $lines[0..($pos -1)], "MyNewLine", $lines[$pos..($lines.Length - 1)]
+
+$newLines | Set-Content .
 
 $arr=( 
-('Angular', 'ok'), 
- ('AngularJs' , 'ok'),
- ('Aws Big Data', 'ok'), 
- ('Aws Machine Learning', 'ok'), 
- ('Building Websites', 'ok'), 
- ('Building Web Apps with React' , 'ok'), 
- ('Java'  , 'ok'), 
- ('Javascript', 'ok'), 
- ('Mysql', 'ok'), 
- ('Powershell', 'non-offical'), 
- ('Python Core', 'ok');
- ('Python Data Analysis','ok')
+('Angular'), 
+ ('AngularJs'),
+ ('Aws Big Data'), 
+ ('Aws Machine Learning'), 
+ ('Building Websites'), 
+ ('Building Web Apps with React' ), 
+ ('Java'  ), 
+ ('Javascript'), 
+ ('Mysql'), 
+ ('Powershell'), 
+ ('Python Core'),
+ ('Python Data Analysis')
  
 )
 
 $output = foreach ($entry in $arr){
-    $link=$($entry[0]).replace(' ', '_').replace(',','').tolower()
+    $link=$($entry).replace(' ', '_').replace(',','').tolower()
     # $link
+    $status=if ($entry -eq 'powershell') {'non-offical'} else { 'ok'}
     [PSCustomObject]@{
-        Name="<a href=$link.md>$($entry[0])</a>"
-        Status=$entry[1] 
+        Name="<a href=$link.md>$entry</a>"
+        Status=$status
     }
 
 }
 
-$HTML='## Pluralsight-quiz-questions
+$output
+
+$html='## Pluralsight-quiz-questions
 <br><p>This repository does not pretend to give you all answers for **Pluralsight questions**, rather it is a starting guide to help you prepare for the skills quiz and to know what to expect.</p><br>'
 
-# $HTML += $output | convertto-HTML
-# $output
-# $HTML -replace ('&lt;','<') -replace ('&gt;','>')|out-file README.md
+$html += $output | convertto-html -title $title
+$html -replace ('&lt;','<') -replace ('&gt;','>')|out-file README.md
+
+# $html
